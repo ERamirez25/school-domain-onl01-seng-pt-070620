814537f36f2ad9e061dd9a23905bde6a17c0b3c5
Elaine Ramirez <elaineramirez208@yahoo.com>
2020-07-25 01:27:02 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -11,34 +11,4 @@ def add_students(student, level)
   roster[level] << student
 end 
 
-def grade(level)
-  roster.detect do |x, y|
-    if x == level
-      return y
-      
-    end 
-  end 
-end 
 
-def sort
-  nu_hash = {}
-  roster.each do |x, y|
-    nu_hash[x] = y.sort
-  end 
- nu_hash
-end 
-
-
-school = School.new("Bayside High School")
-school.roster
-
-school.add_student("Moshe Zeus", 9)
-school.add_student("Onkelos Octavius", 9)
-school.roster
-school.add_student("Sugar Zaza", 5)
-school.roster
-school.add_student("Theo Madus", 5)
-school.add_student("Abbah Zabbah", 2)
-school.roster
-school.grade(9)
-school.sort

249425d649ee7aa6e103b8b11bd2dd35c62c4131
Elaine Ramirez <elaineramirez208@yahoo.com>
2020-07-25 01:35:44 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -9,3 +9,12 @@ def roster
   @roster
 end 
 
+def add_student(name, grade)
+  @student_name = name
+  @grade = grade 
+  if roster.include?(grade) == false
+    @roster[grade] = []
+  end 
+  @roster[grade] << name
+end 
+

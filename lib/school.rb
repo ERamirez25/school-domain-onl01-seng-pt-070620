db8fd41dd1eaa7ab049208018bc7b2bc8603cbdc
Elaine Ramirez <elaineramirez208@yahoo.com>
2020-07-25 01:36:28 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,20 +1,4 @@
 class School
   def initialize
-    @school_name = school_name
-    @roster = {}
-  end 
-end 
-
-def roster
-  @roster
-end 
-
-def add_student(name, grade)
-  @student_name = name
-  @grade = grade 
-  if roster.include?(grade) == false
-    @roster[grade] = []
-  end 
-  @roster[grade] << name
-end 
+  
 
